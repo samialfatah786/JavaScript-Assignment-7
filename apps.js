46a1144061d5e437a80547c845c168a95b458f55
samialfatah786 <samialfatah786@gmail.com>
2023-10-09 01:04:08 +0500

    first commit

---
+++ apps.js
@@ -0,0 +1,13 @@
+
+
+
+
+// function login() {
+//   var name = document.getElementById("name");
+//   var email = document.getElementById("email");
+
+//   if (name.value && email.value) {
+//     console.log(name.value);
+//     console.log(email.value);
+//   } else {
+//     alert("first fill the entire field..");
